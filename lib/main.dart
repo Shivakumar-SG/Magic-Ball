4a528dc3dbd43ccc4cfa3a1df2836f6889ddc0cd
Shivakumar <shivakumarsgangamath@gmail.com>
2021-07-04 09:17:56 +0530

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,53 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.black87,
+          title: Text(
+            'Magic Ball',
+          ),
+        ),
+        body: MagicBall(),
+      ),
+    ),
+  );
+}
+
+class MagicBall extends StatefulWidget {
+  @override
+  _MagicBallState createState() => _MagicBallState();
+}
+
+class _MagicBallState extends State<MagicBall> {
+  int ballNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              child: FlatButton(
+                onPressed: () {
+                  setState(
+                    () {
+                      ballNumber = Random().nextInt(5) + 1;
+                    },
+                  );
+                },
+                child: Image(
+                  image: AssetImage('images/ball$ballNumber.png'),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
+  }
+}
